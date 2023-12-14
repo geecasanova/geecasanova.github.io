17897f8e91466319e66426062ce024a8bda46a8d
geecasanova <145483937+geecasanova@users.noreply.github.com>
2023-12-14 12:14:41 -0500

    Add files via upload

---
+++ ch10/apply/scripts/script.js
@@ -0,0 +1,7 @@
+//Function to display content. 
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
