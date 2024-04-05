e39c13a9266ebe1a9d505d77851235d4cffeeb04
geecasanova <145483937+geecasanova@users.noreply.github.com>
2024-04-05 11:52:58 -0400

    Add files via upload

---
+++ project02/project01-02_txt.js
@@ -0,0 +1,11 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Greg Janvier 
+    Date: 4/3/2024
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
---
+++ project04/project01-04_txt.js
@@ -0,0 +1,12 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Greg Janvier
+    Date:   4/4/2024
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
